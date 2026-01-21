c6222e55e515d1be39540ba4bcee4c6a49ba9110
Rahul Khanke <rahulkhanke786@gmail.com>
2026-01-21 05:52:46 +0000

    Fix: Body hydration warning and improved favicon flame

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -46,20 +46,18 @@ export default function Icon() {
                         }}
                     />
 
-                    {/* Flame Icon */}
+                    {/* Flame Icon - Complete Lucide paths */}
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="20"
                         height="20"
                         viewBox="0 0 24 24"
                         fill="white"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        style={{ zIndex: 10, color: "white" }}
+                        stroke="none"
+                        style={{ zIndex: 10 }}
                     >
-                        <path d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-2.072-4-2.072-5.5C7.928 2 9.928 2 12 2c2.072 0 4.072 2 5 3.5 1.072 1.928 1 3.928 0 6" />
+                        <path d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.5-4.072.5-6 .5-1 1.5-2 2.5-2s2 1 2.5 2c1 1.928 1.572 3.857.5 6-.5 1-1 1.62-1 3a2.5 2.5 0 0 0 2.5 2.5" />
+                        <path d="M12 22c4 0 7-2.5 7-6 0-1.5-.5-3-1.5-4.5-1.5-2.5-2.5-4-2.5-6 0-1.5.5-3 1.5-4.5C13.5 2.5 12.5 2 12 2s-1.5.5-5 4.5C6 7.5 5.5 9 5.5 10.5c0 2 1 3.5 2.5 6C7.5 18.5 7 20 7 21.5c0 .5 0 .5.5.5" />
                     </svg>
                 </div>
             </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} ${spaceGrotesk.variable} ${merienda.variable} font-sans antialiased`}>
+      <body className={`${inter.variable} ${spaceGrotesk.variable} ${merienda.variable} font-sans antialiased`} suppressHydrationWarning>
         {children}
       </body>
     </html>
